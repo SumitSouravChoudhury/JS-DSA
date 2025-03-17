4a1209d5afc5b52b33d286e05a60475a9bda01f9
Sketchbrahma <choudhurysumit377@gmail.com>
2025-03-17 12:17:50 +0530

    new commit

--- Algorithms/SubarrayWithGivenSum.js
+++ Algorithms/SubarrayWithGivenSum.js
@@ -2,27 +2,22 @@ let arr = [2, 6, 3, 5, 8, 9, 3, 2, 2];
 let target = 7;
 
 function subarrayWithGivenSum(arr) {
-  let sum = arr[0];
+  let sum = 0;
   let start = 0;
-  let ans = [];
 
-  for (let i = 1; i <= arr.length; i++) {
-    while (sum > target && start < i - 1) {
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    while (sum > target && start <= i) {
       sum -= arr[start];
       start++;
     }
 
     if (sum === target) {
-      ans.push(start);
-      ans.push(i - 1);
-    }
-
-    if (i < arr.length) {
-      sum += arr[i];
+      return [start, i];
     }
   }
 
-  return ans;
+  return [-1, -1];
 }
 
 console.log(subarrayWithGivenSum(arr));
